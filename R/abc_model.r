9df2dc63bd2669952a6f4b997c0d64646e3a7cb2
Tamborrino <u1875456@live.warwick.ac.uk>
2024-09-12 13:22:33 +0100

    Removed not needed function

--- R/abc_model.r
+++ R/abc_model.r
@@ -5,21 +5,11 @@ NULL
 
 #'@rdname model
 #'@title model
-#'@description Considered models
+#'@description Code for simulating using the splitting scheme from the FHN model
 #'@param theta underlying parameter
-#'@param extra Any additional coefficients/vector of coefficients which may be need to simulate from the model
+#'@param extra Vector with time step delta, initial condition X0 and number of points N to simulate
 #'@return Simulation of observations from all model, returned as matrix;
 #'@export
 model<-function(theta,extra) {
   FHN_model_(theta,extra[1],extra[2:3],extra[4])}
 
-#'@rdname FHNmodel
-#'@title FHNmodel
-#'@description Code for simulating using the splitting scheme from the FHN model
-#'@param theta underlying parameter
-#'@param extra Vector with time step delta, initial condition X0 and number of points N to simulate
-#'@return Simulation of N observations from the FHN model, returned as a matrix;
-#'@export
-FHNmodel<-function(theta,extra) {
-  return(FHN_model_check_(theta,extra[1],extra[2:3],extra[4]))
-}

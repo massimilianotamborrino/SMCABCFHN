06b7e767a01662253096bf07d01c5edcbe4ecb43
Tamborrino <u1875456@live.warwick.ac.uk>
2024-09-12 10:59:17 +0100

    File with the updated names of summaries (canonical and structure-based)

--- R/abc_distance.r
+++ R/abc_distance.r
@@ -7,12 +7,12 @@ NULL
 #'@description Compute the distance given the summaries
 #'@param summaries vector of summaries
 #'@param summ_weights vector with weights of the summaries computed from a pilot study
-#'@param weight weights for the model-based summaries
-#'@param type_sum choose between classic, model based and mixed
+#'@param weight weights for the structure-based summaries
+#'@param type_sum choose between canonical and structure-based
 #'@return ABC distance for the given summaries
 #'@export
 abc_distance<-function(type_sum,summaries,summ_weights,weight) {
-  if(type_sum=='classic') return(sqrt(sum(summaries/diag(summ_weights)* summaries)))
-  else if (type_sum=='model-based') return(summaries[1,1]+weight*summaries[1,2])
+  if(type_sum=='canonical') return(sqrt(sum(summaries/diag(summ_weights)* summaries)))
+  else if (type_sum=='structure-based') return(summaries[1,1]+weight*summaries[1,2])
   }
 
